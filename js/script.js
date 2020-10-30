fff59d0cb748eff60bd873dcc21c8bdf613baee3
Azixp <azimbeledjhem@gmail.com>
2020-10-30 23:02:50 +0100

    portfolio refac

--- js/script.js
+++ js/script.js
@@ -14,20 +14,15 @@ window.addEventListener('scroll', function(){
 
 
 $(document).ready(function(){
-    
-    $(".directional-icon-down a, .nav-container ul a, .directional-icon-up a").on('click', function(event) {
+    $("footer div a, .layout a, .menu-container ul a").on('click', function(event) {
       if (this.hash !== "") {
-
         event.preventDefault();
-  
         var hash = this.hash;
-  
         $('html, body').animate({
           scrollTop: $(hash).offset().top
         }, 800, function(){
-     
           window.location.hash = hash;
         });
-      } 
+      }
     });
-  });
+});
